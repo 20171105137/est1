13112723b1c7be79d6ddb2c21fbabc22b0b224e1
20171105137 <1124891566@qq.com>
2018-07-10 09:08:11 +0800

    文件读写

--- main.cpp
+++ main.cpp
@@ -165,7 +165,165 @@ int main()
 }
 
 
-
+ #include<stdio.h>
+ #include<stdlib.h>
+ int main()
+ {
+ FILE *fp;
+ char ch;
+ char name[20][100];
+ char numbers[20][100];
+ char sex[20][100];
+ char birthofdate[20][100];
+ char cla[20][100];
+ char phNO[20][100];
+ char num[100][100][20];
+ double average[100];
+ int i=0,l=0,m=0,n;
+ int max=0,min=101;
+ fp=fopen("/Users/s20171105112/Desktop/studentdata.csv","r");
+ if(fp==NULL)
+ {
+ printf("??????");
+ }
+ else
+ {
+ ch=fgetc(fp);
+ while(ch!=EOF)
+ {
+ if(i==0){
+ if(ch=='\n')
+ ++i;
+ }
+ if(i!=0){
+ if(l==0){
+ 
+ if(ch==',')
+ {
+ l++;
+ numbers[i-1][m+1]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ numbers[i-1][m]=ch;
+ m++;
+ }
+ else if(l==1)
+ {
+ if(ch==',')
+ {
+ l++;
+ name[i-1][m+1]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ name[i-1][m]=ch;
+ m++;
+ }
+ else if(l==2)
+ {
+ if(ch==',')
+ {
+ l++;
+ sex[i-1][m+1]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ sex[i-1][m]=ch;
+ m++;
+ }
+ else if(l==3)
+ {
+ if(ch==',')
+ {
+ l++;
+ birthofdate[i-1][m+1]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ birthofdate[i-1][m]=ch;
+ m++;
+ }
+ else if(l==4)
+ {
+ if(ch==',')
+ {
+ l++;
+ cla[i-1][m]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ cla[i-1][m]=ch;
+ m++;
+ }
+ else if(l==5)
+ {
+ if(ch==',')
+ {
+ l++;
+ phNO[i-1][m+1]='\0';
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ phNO[i-1][m]=ch;
+ m++;
+ }
+ else{
+ if(ch==',')
+ {
+ num[i-1][l-6][m+1]='\0';
+ average[i-1]+=atof(num[i-1][l-6]);
+ if(max<atoi(num[i-1][l-6]))
+ max=atoi(num[i-1][l-6]);
+ if(min>atoi(num[i-1][l-6]))
+ min=atoi(num[i-1][l-6]);
+ l++;
+ m=0;
+ ch=fgetc(fp);
+ continue;
+ }
+ num[i-1][l-6][m]=ch;
+ m++;
+ if(ch=='\n')
+ {
+ num[i-1][l-6][m+1]='\0';
+ average[i-1]+=atof(num[i-1][l-6]);
+ if(max<atoi(num[i-1][l-6]))
+ max=atoi(num[i-1][l-6]);
+ if(min>atoi(num[i-1][l-6]))
+ min=atoi(num[i-1][l-6]);
+ average[i-1]=(average[i-1]-max-min)/(l-7);
+ max=0;min=101;
+ i++;l=0;m=0;
+ }
+ }
+ }
+ ch=fgetc(fp);
+ }
+ num[i-1][l-6][m+1]='\0';
+ average[i-1]+=atof(num[i-1][l-6]);
+ if(max<atoi(num[i-1][l-6]))
+ max=atoi(num[i-1][l-6]);
+ if(min>atoi(num[i-1][l-6]))
+ min=atoi(num[i-1][l-6]);
+ average[i-1]=(average[i-1]-max-min)/(l-7);
+ fclose(fp);
+ for(n=0;n<i;n++)
+ printf("%s,%s,%s,%s,%s,%s,%.2lf\n",numbers[n],name[n],sex[n],birthofdate[n],cla[n],phNO[n],average[n]);
+ fp=fopen("/Users/s20171105112/biao.csv","w+");
+ fprintf(fp,"%s,%s,%s,%s,%s,%s,%s\n","nummbers","name","sex","dateofbirth","class","phoneNo","average");
+ for(n=0;n<i;n++)
+ fprintf(fp,"%s,%s,%s,%s,%s,%s,%.2lf\n",numbers[n],name[n],sex[n],birthofdate[n],cla[n],phNO[n],average[n]);
+ fclose(fp);
+ }
+ return 0;
+ }
 
 */
 
@@ -181,7 +339,7 @@ int main()
     outFile << "Tom" << ',' << 25 << ',' << "football" << endl;
     outFile << "Jack" << ',' << 21 << ',' << "music" << endl;
     outFile.close();
-    
+ 
     // 读文件
     ifstream inFile("data.csv", ios::in);
     string lineStr;
@@ -199,7 +357,7 @@ int main()
             lineArray.push_back(str);
         strArray.push_back(lineArray);
     }
-    
+ 
     getchar();
     return 0;
 
@@ -232,4 +390,4 @@ int main()
     
 
 }
-   
+
